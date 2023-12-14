9b954aadcbafbc460cdd52356ed334055aa63d9c
NogueiraDan <silvanogueira3460@gmail.com>
2023-12-14 08:17:58 -0300

    feat: Exclui importação não usada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './components/user-list/user-list.component';
-import { UserFormComponent } from './components/user-form/user-form.component';
 import { HomeComponent } from './components/home/home.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
